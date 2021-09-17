356e33b6a6382fc43eb0dc2ffa12d8c57f090861
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-09-16 23:31:44 -0300

    Classe Questão

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,8 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <h1>Teste</h1>
+  );
 }

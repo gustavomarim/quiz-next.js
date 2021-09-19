0f21bd883333f682b7e1f85a778d1f2616b4b379
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-09-19 12:36:14 -0300

    Banco de Questões

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -1,6 +1,5 @@
+import questoes from "./bancoDeQuestoes";
+
 export default (req, res) => {
-    res.status(200).json({
-        id: +req.query.id,
-        name: 'Jéssica'
-    });
+    res.status(200).json(questoes[0]);
 }

c438a953571c1a566d424ce4a12ba8f6a37e070d
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-10-09 19:45:41 -0300

    Componente Questionário #02

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   const [questao, setQuestao] = useState(questaoMock);
 
   function questaoRespondida(questao: QuestaoModel) {
-
+    
   };
 
   function irPraProximoPasso() {
@@ -43,7 +43,7 @@ export default function Home() {
     }}>
       <Questionario
         questao={questao}
-        ultima={true}
+        ultima={false}
         questaoRespondida={questaoRespondida}
         irPraProximoPasso={irPraProximoPasso}
       />

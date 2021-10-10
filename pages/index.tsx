b2768015b996657dd7b64cb67898e50124235d43
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-10-10 18:40:25 -0300

    Componente Index #03

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,15 @@
 import { useEffect, useState } from 'react';
 import Questionario from '../components/Questionario';
 import QuestaoModel from '../model/questao';
-import RespostaModel from '../model/resposta';
-
-const questaoMock = new QuestaoModel(1, "Melhor cor?", [
-  RespostaModel.errada("Verde"),
-  RespostaModel.errada("Vermelha"),
-  RespostaModel.errada("Azul"),
-  RespostaModel.certa("Preta"),
-]);
+import { useRouter } from "next/router";
 
 const BASE_URL = "http://localhost:3000/api";
 
 export default function Home() {
+  const router = useRouter();
+
   const [idsDasQuestoes, setidsDasQuestoes] = useState<number[]>([]);
-  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
+  const [questao, setQuestao] = useState<QuestaoModel>();
   const [respostasCertas, setrespostasCertas] = useState<number>(0);
 
   async function carregarIdsDasQuestoes() {
@@ -46,14 +41,36 @@ export default function Home() {
     setrespostasCertas(respostasCertas + (acertou ? 1 : 0));
   };
 
+  function idProximaPergunta() {
+    if (questao) {
+      const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1;
+      return idsDasQuestoes[proximoIndice];
+    }
+  };
+
   function irPraProximoPasso() {
-    
+    const proximoId = idProximaPergunta();
+    proximoId ? irPraProximaQuestao(proximoId) : finalizar();
+  };
+
+  function irPraProximaQuestao(proximoId: number) {
+    carregarQuestao(proximoId);
+  };
+
+  function finalizar() {
+    router.push({
+      pathname: "/resultado",
+      query: {
+        total: idsDasQuestoes.length,
+        certas: respostasCertas,
+      },
+    });
   };
 
   return (
     <Questionario
       questao={questao}
-      ultima={false}
+      ultima={idProximaPergunta() === undefined}
       questaoRespondida={questaoRespondida}
       irPraProximoPasso={irPraProximoPasso}
     />

0ede30abe4f127422b317aa93e473a7832a2e33a
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-10-12 17:08:54 -0300

    Animação Resposta

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -10,14 +10,14 @@ interface RespostaProps {
 };
 
 export default function Resposta(props: RespostaProps) {
-    const resposta = props.valor
+    const resposta = props.valor;
+    const respostaRevelada = resposta.revelada ? styles.respostaRevelada : "";
 
     return (
         <div className={styles.resposta}
             onClick={() => props.respostaFornecida(props.indice)}>
-            <div className={styles.conteudoResposta}>
-                {!resposta.revelada ? (
-                 <div className={styles.frente}>
+            <div className={`${respostaRevelada} ${styles.conteudoResposta}`}>
+                <div className={styles.frente}>
                     <div className={styles.letra}
                         style={{ backgroundColor: props.corFundoLetra }}>
                         {props.letra}
@@ -26,7 +26,6 @@ export default function Resposta(props: RespostaProps) {
                         {resposta.valor}
                     </div>
                 </div>
-                ): (
                 <div className={styles.verso}>
                     {resposta.certa ? (
                         <div className={styles.certa}>
@@ -40,7 +39,6 @@ export default function Resposta(props: RespostaProps) {
                         </div>
                     )}
                 </div>
-                )}
             </div>
         </div>
     )

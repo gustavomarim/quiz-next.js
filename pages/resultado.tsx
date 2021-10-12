a0d9ea639c51289930fdf1009f75447b727cbb54
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2021-10-12 16:53:23 -0300

    Componente Estatística

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,5 +1,7 @@
 import styles from "../styles/Resultado.module.css";
 import { useRouter } from "next/router";
+import Botao from "../components/Botao";
+import Estatistica from "../components/Estatistica";
 
 export default function resultado() {
 
@@ -9,12 +11,30 @@ export default function resultado() {
     const certas = +router.query.certas;
     const percentual = Math.round((certas / total) * 100);
 
+    let corFundoPercentualAcertos = () => {
+        if (percentual <= 33) {
+            return "#de6a33";
+        } else if (percentual >= 33 && percentual <= 66) {
+            return "#ffa500 ";
+        } else {
+            return "#9CD2A4";
+        }
+    };
+
     return (
         <div className={styles.resultado}>
             <h1>Resultado Final</h1>
-            <div>{total}</div>
-            <div>{certas}</div>
-            <div>{`${percentual}%`}</div>
+            <div style={{
+                display: "flex",
+            }}>
+                <Estatistica texto="Perguntas" valor={total} />
+                <Estatistica texto="Respostas Certas" valor={certas}
+                    corFundo="#9CD2A4" />
+                <Estatistica texto="Percentual de Acertos" valor={`${percentual}%`}
+                    corFundo={corFundoPercentualAcertos()}
+                />
+            </div>
+            <Botao href="/" texto="Tentar Novamente" />
         </div>
     );
 };
